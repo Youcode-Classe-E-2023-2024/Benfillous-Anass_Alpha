4d63adcfe46032ae10d22b9ff228ef6545838389
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-03 15:14:50 +0100

    add login

---
+++ views/login_view.php
@@ -0,0 +1,27 @@
+<div class="background">
+    <div class="shape"></div>
+    <div class="shape"></div>
+</div>
+<form method="post" action="index.php?page=login" class="form">
+    <h3>Login Here</h3>
+
+    <label for="username">Username</label>
+    <input class="login-input" name="email" type="text" placeholder="Email" id="username">
+
+    <label for="password">Password</label>
+    <input class="login-input" name="password" type="password" placeholder="Password" id="password">
+
+    <div class="mt-4">
+        <a href="index.php?page=password_recovery">Forgotten password?</a>
+    </div>
+    
+    <div class="social flex justify-center ">
+        <button name="login" id="btn">Login</button>
+    </div>
+    <div class="social items-center flex-col">
+        <p class="text-sm mb-4">Or</p>
+        <a href="index.php?page=register">
+            <div class="cursor-pointer">Sign Up</div>
+        </a>
+    </div>
+</form>
