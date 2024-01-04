e938b61b9986bca4c067f3b3db3ee06ffd1dd149
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-04 08:10:03 +0100

    update api consumption

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -9,3 +9,12 @@ if (isset($_POST['logout'])) {
     $authentication = new Authentication();
     $authentication->logout();
 }
+
+
+if (isset($_POST["products"])) {
+    header("Access-Control-Allow-Origin: *");
+    $url = "https://jsonplaceholder.typicode.com/posts";
+    $data = file_get_contents($url);
+    echo $data;
+    exit;
+}
