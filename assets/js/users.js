2efeda1c10b915b15a82a6ad33c69057c1b2fab2
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-04 10:46:21 +0100

    update users list

---
+++ assets/js/users.js
@@ -0,0 +1,33 @@
+const usersSection = document.getElementById("users-list-section");
+
+function getUsers() {
+    $.ajax({
+        type: "POST",
+        url: "index.php?page=dashboard",
+        data: {users: true},
+        success: (data) => {
+            let users = JSON.parse(data);
+            console.log(users);
+
+            users.forEach((user) => {
+                usersSection.innerHTML += `
+        <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                <!-- Team Thumb-->
+                <div class="advisor_thumb"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                    <!-- Social Info-->
+                    <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                </div>
+                <!-- Team Details-->
+                <div class="single_advisor_details_info">
+                    <h6>${user.name}</h6>
+                    <p class="designation">${user.email}</p>
+                </div>
+            </div>
+        </div>`;
+            })
+        }
+    })
+}
+
+/*getUsers();*/
---
+++ views/users_view.php
@@ -0,0 +1,5 @@
+<div class="h-auto p-4 hidden">
+    <div id="users-list-section" class="row grid-col-5 gap-y-4">
+    </div>
+
+</div>
