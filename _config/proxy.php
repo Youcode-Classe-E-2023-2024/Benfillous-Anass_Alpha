731239ccb24d25cf15b3237f1f078088aa616f96
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-04 08:08:38 +0100

    remove unwanted file

--- _config/proxy.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_POST["products"])) {
-    header("Access-Control-Allow-Origin: *");
-    $url = "https://jsonplaceholder.typicode.com/posts";
-    $data = file_get_contents($url);
-    echo $data;
-    exit;
-}
