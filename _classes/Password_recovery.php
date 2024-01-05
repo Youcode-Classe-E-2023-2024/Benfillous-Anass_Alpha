542d44e66870cb5d4989703e9ff02299b11d4ac2
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-05 12:59:16 +0100

    reset passwd recovery methods to static methods

--- _classes/Password_recovery.php
+++ _classes/Password_recovery.php
@@ -1,24 +1,40 @@
 <?php
 class Password_recovery {
-    function checkRequest($email) {
+    static function checkRequest($selector, $col) {
         global $db;
-        $sql = "SELECT * FROM password_recovery WHERE pwd_reset_email=?";
+        $sql = "SELECT * FROM password_recovery WHERE `$col`=?";
+        $stmt = mysqli_stmt_init($db);
+        mysqli_stmt_prepare($stmt, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $selector);
+        mysqli_stmt_execute($stmt);
+        $res = mysqli_stmt_get_result($stmt);
+
+        return mysqli_fetch_assoc($res);
+    }
+
+    static function checkUser($email) {
+        global $db;
+        $sql = "SELECT * FROM user WHERE email=?";
         $stmt = mysqli_stmt_init($db);
         mysqli_stmt_prepare($stmt, $sql);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
+
+        $res = mysqli_stmt_get_result($stmt);
+
+        return mysqli_fetch_assoc($res);
     }
 
-    function addRequest($email, $selector, $token, $date) {
+    static function addRequest($email, $selector, $token, $date) {
         global $db;
-        $sql = "INSET INTO password_recovery (pwd_reset_email, pwd_reset_selector, pwd_reset_token, pwd_reset_expires) VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO password_recovery (pwd_reset_email, pwd_reset_selector, pwd_reset_token, pwd_reset_expires) VALUES (?, ?, ?, ?)";
         $stmt = mysqli_stmt_init($db);
         mysqli_stmt_prepare($stmt, $sql);
         mysqli_stmt_bind_param($stmt, "sssi", $email, $selector, $token, $date);
         mysqli_stmt_execute($stmt);
     }
 
-    function deleteRequest($selector) {
+    static function deleteRequest($selector) {
         global $db;
         $sql = "DELETE FROM password_recovery WHERE pwd_reset_selector=?";
         $stmt = mysqli_stmt_init($db);
@@ -27,12 +43,12 @@ class Password_recovery {
         mysqli_stmt_execute($stmt);
     }
 
-    function updatePassword($email, $password) {
+    static function updatePassword($email, $password) {
         global $db;
         $sql = "UPDATE user SET password=? WHERE email=?";
         $stmt = mysqli_stmt_init($db);
         mysqli_stmt_prepare($stmt, $sql);
-        mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+        mysqli_stmt_bind_param($stmt, "ss", $password, $email);
         mysqli_stmt_execute($stmt);
     }
 }
