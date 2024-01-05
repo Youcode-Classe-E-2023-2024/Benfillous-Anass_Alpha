f0010798766bad3c32990eaae57618402cd64c7a
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-05 10:14:40 +0100

    update users view

--- views/users_view.php
+++ views/users_view.php
@@ -1,4 +1,18 @@
 <div id="users-container" class="hidden h-auto p-4">
+    <h3 class="text-center mb-4">Add New User</h3>
+    <div id="user-add-btn" class="mb-3 cursor-pointer w-auto h-auto">
+        <div class="flex-1 h-full">
+            <div class="flex items-center justify-center flex-1 h-full p-2 bg-blue-800 text-white shadow rounded-full">
+                <div class="relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="users-list-section" class="row grid-col-5 gap-y-4">
     </div>
 </div>
