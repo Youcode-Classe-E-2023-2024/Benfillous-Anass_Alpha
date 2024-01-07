ff13e5a4b7c26e9a75979399177c9c53c54cf19b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-01-07 22:48:46 +0100

    update db sql

--- _config/database.sql
+++ _config/database.sql
@@ -1,12 +1,33 @@
-create table users
+create table password_recovery
 (
-    users_id       bigint auto_increment
+    pwd_reset_id       int(6) unsigned auto_increment
         primary key,
-    users_email    varchar(255) not null,
-    users_password text         not null,
-    users_username varchar(255) null,
-    constraint users_users_email_uindex
-        unique (users_email),
-    constraint users_users_id_uindex
-        unique (users_id)
+    pwd_reset_email    varchar(100) not null,
+    pwd_reset_selector varchar(50)  not null,
+    pwd_reset_token    text         not null,
+    pwd_reset_expires  bigint       not null
 );
+
+create table user
+(
+    user_id  int auto_increment
+        primary key,
+    username varchar(25)  null,
+    email    varchar(25)  null,
+    password varchar(250) null,
+    picture  text         null
+);
+
+create table notification
+(
+    notification_id int auto_increment
+        primary key,
+    notification    text                 null,
+    user_id         int                  null,
+    seen            tinyint(1) default 0 null,
+    constraint notification_ibfk_1
+        foreign key (user_id) references user (user_id)
+);
+
+create index user_id
+    on notification (user_id);
